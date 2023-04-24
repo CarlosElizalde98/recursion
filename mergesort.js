76a16efad28b229cfd717b823aea3dcc30f05ba1
Carlos E <58950500+CarlosElizalde98@users.noreply.github.com>
2023-04-24 12:59:06 -0500

    added solution to merge sort

--- mergesort.js
+++ mergesort.js
@@ -6,40 +6,26 @@ function mergeSort(array) {
   //sort right half of elements
   //merge sorted halves
   let copy = array;
-  if (copy.length > 2) {
-    let mid = Math.floor(copy.length / 2);
-    let arr1 = copy.slice(1, mid);
-    mergeSort(arr1);
-    let arr2 = copy.slice(mid + 1, copy.length);
-    mergeSort(arr2);
-    merge(arr1, arr2, arr1.length, arr2.length);
-  } else {
+  if (copy.length <= 1) {
     return copy;
   }
+  let mid = Math.floor(copy.length / 2);
+  let left = mergeSort(copy.slice(0, mid));
+  let right = mergeSort(copy.slice(mid));
+
+  return merge(left, right);
 }
 
-function merge(arr1, arr2, firstL, secondL) {
-  let a = firstL;
-  let b = secondL;
-
-  let [i, j, k] = [1, 1, 1];
+function merge(arr1, arr2) {
   let sorted = [];
-
-  while (i < a && j < b) {
-    if (arr1[i] < arr2[j]) {
-      sorted[k++] = arr1[i++];
+  while (arr1.length && arr2.length) {
+    if (arr1[0] < arr2[0]) {
+      sorted.push(arr1.shift());
     } else {
-      sorted[k++] = arr2[j++];
+      sorted.push(arr2.shift());
     }
   }
-  for (i; i <= a; i++) {
-    sorted[k++] = arr1[i];
-  }
-  for (j; j <= b; j++) {
-    sorted[k++] = arr2[j];
-  }
-
-  return sorted;
+  return sorted.concat(arr1.slice().concat(arr2.slice()));
 }
 
 module.exports = mergeSort;
--- mergesort.test.js
+++ mergesort.test.js
@@ -4,5 +4,5 @@ const unsorted = [2, 5, 6, 8, 1, 3, 4, 7];
 const sorted = [1, 2, 3, 4, 5, 6, 7, 8];
 
 test("Expect the array to be sorted", () => {
-  expect(mergeSort(unsorted)).toBe(sorted);
+  expect(mergeSort(unsorted)).toEqual(sorted);
 });
