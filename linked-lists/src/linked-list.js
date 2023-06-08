5f38996310a964d2d83d897e07c2ff6bd548c2d2
Carlos E <58950500+CarlosElizalde98@users.noreply.github.com>
2023-06-08 10:47:15 -0500

    initialize npm and added methods to LinkedList class

---
+++ linked-lists/src/linked-list.js
@@ -0,0 +1,63 @@
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  prepend(data) {
+    const newNode = new Node(data, this.head);
+    this.head = newNode;
+    length++;
+  }
+
+  append(data) {
+    let oldTail = this.tail();
+    let newTail = new Node(data);
+    oldTail.nextNode = newTail;
+  }
+
+  size() {
+    return this.length;
+  }
+
+  head() {
+    return this.head;
+  }
+
+  tail() {
+    let current = this.head;
+    while (current.nextNode !== null) {
+      current = current.nextNode;
+    }
+    return current;
+  }
+
+  getAtIndex(index) {
+    if (index < 0 || index >= this.length) {
+      return null;
+    }
+
+    let current = this.head;
+    for (let i = 0; i < index; i++) {
+      current = current.nextNode;
+    }
+    return current;
+  }
+
+  toString() {
+    let output = ``;
+    let current = this.head;
+    while (current) {
+      let output = `${output}${current.value} ->`;
+      current = current.nextNode;
+    }
+    console.log(`${output} null`);
+  }
+}
+
+class Node {
+  constructor(value, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
